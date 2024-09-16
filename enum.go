26c2533b0c3955d98651a52ffc0673e1c53a028c
Mohammed Sohail <sohailsameja@gmail.com>
2024-09-16 14:31:17 +0300

    feat: add updated enum

--- enum.go
+++ enum.go
@@ -1,16 +1,31 @@
 package fonbnk
 
 type (
-	Country     string
-	OffRampType string
+	Country            string
+	Network            string
+	OffRampType        string
+	OffRampCurrency    string
+	OffRampAsset       string
+	OffRampPaymentType string
 )
 
 const (
 	KENYA  Country = "KE"
 	UGANDA Country = "UG"
 
+	CELO Network = "CELO"
+
 	BANK         OffRampType = "bank"
 	AIRTIME      OffRampType = "airtime"
 	MOBILE_MONEY OffRampType = "mobile_money"
 	PAYBILL      OffRampType = "paybill"
+
+	LOCAL OffRampCurrency = "local"
+	USD   OffRampCurrency = "usd"
+
+	CUSD OffRampAsset = "CUSD"
+	USDT OffRampAsset = "USDT"
+	USDC OffRampAsset = "USDC"
+
+	CRYPTO OffRampPaymentType = "CRYPTO_WALLET"
 )
